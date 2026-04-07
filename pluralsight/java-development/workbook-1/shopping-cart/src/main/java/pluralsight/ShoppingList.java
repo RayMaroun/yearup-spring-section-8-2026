373fbf6c903c65a18836320363569ea151591a19
RayMaroun <raymondmaroun15@gmail.com>
2026-04-07 16:02:16 +0300

    Update Workbook 1

---
+++ pluralsight/java-development/workbook-1/shopping-cart/src/main/java/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My Shopping List:");
+        System.out.println("- Apples");
+        System.out.println("- Bananas");
+        System.out.println("- Bread");
+        System.out.println("- Milk");
+        System.out.println("- Cheese");
+        System.out.println("- Eggs");
+        System.out.println("- Chicken");
+        System.out.println("- Rice");
+        System.out.println("- Potato");
+        System.out.println("- Tomatoes");
+    }
+}
