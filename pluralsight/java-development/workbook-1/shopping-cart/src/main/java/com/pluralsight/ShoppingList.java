86bd5c52ae4cf88ef28c4cb672fca3856f08b7c5
RayMaroun <raymondmaroun15@gmail.com>
2026-04-08 18:18:01 +0300

    Update Workbook 1

--- pluralsight/java-development/workbook-1/shopping-cart/src/main/java/com/pluralsight/ShoppingList.java
+++ pluralsight/java-development/workbook-1/shopping-cart/src/main/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,4 @@
-package pluralsight;
+package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
--- pluralsight/java-development/workbook-1/string-application/src/main/java/com/pluralsight/VariableApp.java
+++ pluralsight/java-development/workbook-1/string-application/src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,4 @@
-package pluralsight;
+package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
