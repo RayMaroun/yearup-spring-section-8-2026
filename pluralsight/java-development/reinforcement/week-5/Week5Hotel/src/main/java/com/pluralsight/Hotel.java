ee7fbeb26f487b12d11781b9e19791d2aadef992
RayMaroun <raymondmaroun15@gmail.com>
2026-05-07 21:32:36 +0300

    Update reinforcement lab

---
+++ pluralsight/java-development/reinforcement/week-5/Week5Hotel/src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Hotel {
+    private static int totalBookings = 0;
+
+    private Hotel() {
+    }
+
+    public static int getTotalBookings() {
+        return totalBookings;
+    }
+
+    public static void increaseTotalBookings() {
+        totalBookings++;
+    }
+
+    public static void decreaseTotalBookings() {
+        if (totalBookings > 0) {
+            totalBookings--;
+        }
+    }
+}
