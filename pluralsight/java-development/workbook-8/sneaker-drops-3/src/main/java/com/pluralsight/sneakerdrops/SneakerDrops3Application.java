d3589cd702dee0851bc2595853f81c96090a0601
RayMaroun <raymondmaroun15@gmail.com>
2026-06-09 21:11:01 +0300

    Update workbook 8

---
+++ pluralsight/java-development/workbook-8/sneaker-drops-3/src/main/java/com/pluralsight/sneakerdrops/SneakerDrops3Application.java
@@ -0,0 +1,12 @@
+package com.pluralsight.sneakerdrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerDrops3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerDrops3Application.class, args);
+    }
+}
